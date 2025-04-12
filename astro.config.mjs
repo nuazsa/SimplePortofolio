1b6c3f8a2cf46dcba06e0f048cb327ab7e8c11ec
Nur Azis Saputra <nurazissaputra@gmail.com>
2025-04-12 21:28:39 +0700

    Configure for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import 'dotenv/config';
 
-const isDev = process.env.NODE_ENV === 'development';
-const base = isDev ? '/' : (process.env.BASE || '/SimplePortofolio');
+const base = process.env.BASE ?? 'https://nuazsa.github.io/SimplePortofolio/';
 
 export default defineConfig({
   site: 'https://nuazsa.github.io/SimplePortofolio/',
